893848fde741d021dbb29b6a125c113f523095c7
William Nicholson <will98nicholson@gmail.com>
2021-04-28 22:24:53 -0400

    models

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type: DataTypes.TEXT("long"),
+            allowNull: false,
+
+
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Post = require('./Post');
+// const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id'
+// });
+// Post.hasMany(Comment, {
+//     foreignKey: 'post_id'
+// });
+
+
+module.exports = { User, Post, }
