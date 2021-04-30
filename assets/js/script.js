ec738f2ce72faaf761fcbafb1afe5f7b15c058a4
Morgan Schall <morgan.allison.schall@gmail.com>
2021-04-29 20:54:15 -0400

    added the fetch call for the API

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+//log in log out pages 
+//profiles 
+
+//TV API 
+const searchEl = document.querySelector('#keyword');
+const searchbtn = document.querySelector('.btn');
+const results = document.querySelector('.results');
+//remember to add or change these based on class/id we use in the page
+
+function callApi(){
+    const searchWord = searchEl.value
+
+    const search = searchWord.replace(" ", "%20")
+
+    const tvAPI = 'http://api.tvmaze.com/search/shows?q=' + search;
+    //we will have to see how it fetches and stores data
+    //singlesearch brings up the #1 just search brings them all 
+
+    console.log(searchWord);
+    console.log(search);
+    console.log(tvAPI);
+
+
+    
+    fetch(tvAPI)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+        results.innerHTML = '';
+            for (let i = 0; i < data.length ; i++) {
+                let showName = data[i].show.name;
+                let showSum = data[i].show.summary
+                console.log(showName)
+                const showsContainer = `<h2>${showName}</h2> ${showSum}`
+                results.innerHTML += showsContainer;
+           }
+            return;
+    })
+
+    
+}
+
+
+searchbtn.addEventListener('click', () => {
+    callApi();
+});
