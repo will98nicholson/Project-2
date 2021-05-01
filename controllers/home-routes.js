9bb2631f9dbded2028318fe65c3e899bf787e654
Morgan Schall <morgan.allison.schall@gmail.com>
2021-05-01 10:39:56 -0400

    updated read me added login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,5 +9,13 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+   if (req.session.loggedIn) {
+     res.redirect('/');
+     return;
+   }
+
+  res.render('login');
+});
 
 module.exports = router;
