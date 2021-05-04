83041d114b6fbde31c45a67e4059fbf95d221f0f
William Nicholson <will98nicholson@gmail.com>
2021-05-04 18:16:59 -0400

    display login bars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,10 +10,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-   if (req.session.loggedIn) {
-     res.redirect('/');
-     return;
-   }
+
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
   res.render('login');
 });
