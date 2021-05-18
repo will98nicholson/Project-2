56f72f87bd9743b3638239792781b1af0466ebd1
William Nicholson <will98nicholson@gmail.com>
2021-05-18 18:45:28 -0500

    compression

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/auth');
+const compression = require('compression')
+const express = require('express')
+const app = express()
+app.use(compression({ filter: shouldCompress }))
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -26,6 +30,7 @@ const sess = {
 app.use(session(sess));
 
 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -34,6 +39,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
