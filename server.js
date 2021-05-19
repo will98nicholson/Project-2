251731dcdbd91c1e597a6d217d7bf164c15cfe71
William Nicholson <will98nicholson@gmail.com>
2021-05-18 19:14:27 -0500

    compression fix

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/auth');
-// const compression = require('compression')
-// const express = require('express')
-// const app = express()
-// app.use(compression({ filter: shouldCompress }))
+const compression = require('compression')
+
+
+const app = express()
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
